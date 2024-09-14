c1541b911332e205a0cb5b34a03a2954bae1e176
xuqianzhi <xuqianzhi1998@hotmail.com>
2024-09-14 19:39:27 -0400

    React Viro AR initial setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,56 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import {
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroText,
+  ViroTrackingReason,
+  ViroTrackingStateConstants,
+} from "@reactvision/react-viro";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState("Initializing AR...");
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    console.log("onInitialized", state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText("Hello World!");
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      // Handle loss of tracking
+    }
+  }
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    </ViroARScene>
   );
-}
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+});
